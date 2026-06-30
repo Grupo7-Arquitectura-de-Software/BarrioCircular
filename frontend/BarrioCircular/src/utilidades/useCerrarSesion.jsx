afdc5c9397aec5098d66c20ce581257a6781dd44
wfap <217279580+wfandrangop@users.noreply.github.com>
2026-06-30 18:57:10 -0500

    feat(frontend): implementacion cierre de sesion
    Se implementa el cierre de sesion para manejar el fin de acceso de una sesion de un usuario. Se lo realiza a nivel de frontend ya que clerk se encarga de gestionar la autenticacion.
    
    ISSUE: 23

---
+++ frontend/BarrioCircular/src/utilidades/useCerrarSesion.jsx
@@ -0,0 +1,17 @@
+import { useClerk } from "@clerk/clerk-react";
+import { useNavigate } from "react-router-dom";
+
+const RUTA_AUTENTICACION = "/auth";
+
+export const useCerrarSesion = () => {
+    const { signOut } = useClerk();
+    const navigate = useNavigate();
+
+    return async () => {
+        try {
+            await signOut();
+        } finally {
+            navigate(RUTA_AUTENTICACION, { replace: true });
+        }
+    };
+};
